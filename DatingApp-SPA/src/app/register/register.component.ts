f874418ee61493fc41e90c3b536cdd430921bf84
Ragenaut Bladehelm <simeonvkolev@gmail.com>
2020-06-03 14:19:22 +0200

    improved code and ui with third party libs

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,16 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register(){
    this.authService.register(this.model).subscribe(() => {
-     console.log('success');
+     this.alertify.success('registered');
    }, error => {
-     console.log(error);
+     this.alertify.error(error);
    });
   }
 
